971ed96cacfe3f43563a567fbedf76bef7489163
Adrian Price-Whelan <adrian.prw@gmail.com>
2015-05-28 00:40:25 -0400

    add value to all

--- biff/bfe.pyx
+++ biff/bfe.pyx
@@ -26,7 +26,7 @@ cdef extern from "gsl/gsl_sf_gamma.h":
     double gsl_sf_lngamma(double x) nogil
     double gsl_sf_fact(unsigned int n) nogil
 
-__all__ = ['acceleration']
+__all__ = ['acceleration', 'value']
 
 cpdef acceleration(double[:,::1] xyz, double[:,::1] acc,
                    double[:,:,::1] sin_coeff, double[:,:,::1] cos_coeff,
